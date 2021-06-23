a19a89258cd8ee7ae003b70bf1bc4dc63c8ac6df
Rakesh <iamrakeshkumar@pm.me>
2021-06-23 16:09:20 +0530

    testing:refactoring tests

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -2,40 +2,31 @@
 import cases from 'jest-in-case'
 import {isPasswordAllowed} from '../auth'
 
+function casify(obj) {
+  return Object.entries(obj).map(([name, password]) => ({
+    name: `${password}-${name}`,
+    password,
+  }))
+}
+
 cases(
   `isPasswordAllowed:valid passwords`,
   (options) => {
     expect(isPasswordAllowed(options.password)).toBe(true)
   },
-  {
-    'valid password': {
-      password: '!aBc123',
-    },
-  },
+  casify({'valid Password': '!aBc123'}),
 )
 cases(
   `isPasswordAllowed: invalid passwords`,
   (options) => {
     expect(isPasswordAllowed(options.password)).toBe(false)
   },
-  {
-    'too short': {
-      password: 'a2c!',
-    },
-    'no letters': {
-      password: '123456!',
-    },
-    'no numbers': {
-      password: 'ABCdef!',
-    },
-    'no uppercase letters': {
-      password: 'abc123!',
-    },
-    'no lowercase letters': {
-      password: 'ABC123!',
-    },
-    'no non-alphanumeric characters': {
-      password: 'ABCdef123',
-    },
-  },
+  casify({
+    'too short': 'a2c!',
+    'no letters': '123456!',
+    'no numbers': 'ABCdef!',
+    'no uppercase letters': 'abc123!',
+    'no lowercase letters': 'ABC123!',
+    'no non-alphanumeric characters': 'ABCdef123',
+  }),
 )
