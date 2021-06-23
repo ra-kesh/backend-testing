674ef9061608a445a154383eaa77f245ca22f1d9
Rakesh <iamrakeshkumar@pm.me>
2021-06-23 14:31:36 +0530

    testing:pure functions with exra abstraction

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,15 +1,38 @@
 // Testing Pure Functions
 import {isPasswordAllowed} from '../auth'
 
-test('isPasswordAllowed returns true for valid passwords', () => {
-  expect(isPasswordAllowed('!aBc123')).toBe(true)
+const allowedPasswords = ['!aBc123']
+
+const notAllowedPasswords = [
+  'a2c!',
+  '123456!',
+  'ABCdef!',
+  'abc123!',
+  'ABC123!',
+  'ABCdef123',
+]
+
+allowedPasswords.forEach((password) => {
+  test(`allows ${password}`, () => {
+    expect(isPasswordAllowed(password)).toBe(true)
+  })
 })
 
-test('isPasswordAllowed returns false for invalid passwords', () => {
-  expect(isPasswordAllowed('a2c!')).toBe(false)
-  expect(isPasswordAllowed('123456!')).toBe(false)
-  expect(isPasswordAllowed('ABCdef!')).toBe(false)
-  expect(isPasswordAllowed('abc123!')).toBe(false)
-  expect(isPasswordAllowed('ABC123!')).toBe(false)
-  expect(isPasswordAllowed('ABCdef123')).toBe(false)
+notAllowedPasswords.forEach((password) => {
+  test(`not allows ${password}`, () => {
+    expect(isPasswordAllowed(password)).toBe(false)
+  })
 })
+
+// test('isPasswordAllowed returns true for valid passwords', () => {
+//   expect(isPasswordAllowed('!aBc123')).toBe(true)
+// })
+
+// test('isPasswordAllowed returns false for invalid passwords', () => {
+//   expect(isPasswordAllowed('a2c!')).toBe(false)
+//   expect(isPasswordAllowed('123456!')).toBe(false)
+//   expect(isPasswordAllowed('ABCdef!')).toBe(false)
+//   expect(isPasswordAllowed('abc123!')).toBe(false)
+//   expect(isPasswordAllowed('ABC123!')).toBe(false)
+//   expect(isPasswordAllowed('ABCdef123')).toBe(false)
+// })
