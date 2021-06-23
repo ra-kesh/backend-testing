b8b6aa4afde5b3059200b9c90c314eab80beb27a
Rakesh <iamrakeshkumar@pm.me>
2021-06-23 15:52:18 +0530

    testing:jest in case added

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,38 +1,41 @@
 // Testing Pure Functions
+import cases from 'jest-in-case'
 import {isPasswordAllowed} from '../auth'
 
-const allowedPasswords = ['!aBc123']
-
-const notAllowedPasswords = [
-  'a2c!',
-  '123456!',
-  'ABCdef!',
-  'abc123!',
-  'ABC123!',
-  'ABCdef123',
-]
-
-allowedPasswords.forEach((password) => {
-  test(`allows ${password}`, () => {
-    expect(isPasswordAllowed(password)).toBe(true)
-  })
-})
-
-notAllowedPasswords.forEach((password) => {
-  test(`not allows ${password}`, () => {
-    expect(isPasswordAllowed(password)).toBe(false)
-  })
-})
-
-// test('isPasswordAllowed returns true for valid passwords', () => {
-//   expect(isPasswordAllowed('!aBc123')).toBe(true)
-// })
-
-// test('isPasswordAllowed returns false for invalid passwords', () => {
-//   expect(isPasswordAllowed('a2c!')).toBe(false)
-//   expect(isPasswordAllowed('123456!')).toBe(false)
-//   expect(isPasswordAllowed('ABCdef!')).toBe(false)
-//   expect(isPasswordAllowed('abc123!')).toBe(false)
-//   expect(isPasswordAllowed('ABC123!')).toBe(false)
-//   expect(isPasswordAllowed('ABCdef123')).toBe(false)
-// })
+cases(
+  `isPasswordAllowed:valid passwords`,
+  (options) => {
+    expect(isPasswordAllowed(options.password)).toBe(true)
+  },
+  {
+    'valid password': {
+      password: '!aBc123',
+    },
+  },
+)
+cases(
+  `isPasswordAllowed: invalid passwords`,
+  (options) => {
+    expect(isPasswordAllowed(options.password)).toBe(false)
+  },
+  {
+    'too short': {
+      password: 'a2c!',
+    },
+    'no letters': {
+      password: '123456!',
+    },
+    'no numbers': {
+      password: 'ABCdef!',
+    },
+    'no uppercase letters': {
+      password: 'abc123!',
+    },
+    'no lowercase letters': {
+      password: 'ABC123!',
+    },
+    'no non-alphanumeric characters': {
+      password: 'ABCdef123',
+    },
+  },
+)
